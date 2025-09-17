90e92d97d492cc702834082a32e5e10947c91a02
maxjo020418 <maxjo020418@gmail.com>
2025-09-17 23:49:29 +0900

    changed domain

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 from __future__ import annotations
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Request, Response
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api import applications, auth, bounties, webhooks
-from app.config import get_settings
-
-settings = get_settings()
 
 app = FastAPI(title="Headhunt Bounty API", version="0.1.0")
 
+ALLOWED_ORIGINS = [
+    "https://mihari-temp.yeongmin.net",
+    "http://mihari-temp.yeongmin.net",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://mihari.yeongmin.net",
-        "http://mihari.yeongmin.net",
-    ],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,6 +24,22 @@ app.add_middleware(
 @app.options("/{path:path}", include_in_schema=False)
 async def handle_options(path: str) -> Response:
     return Response(status_code=200)
+
+
+@app.middleware("http")
+async def ensure_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    origin = request.headers.get("origin")
+    allow_origin = origin if origin in ALLOWED_ORIGINS else ALLOWED_ORIGINS[0]
+    if "access-control-allow-origin" not in response.headers:
+        response.headers["Access-Control-Allow-Origin"] = allow_origin
+    if "access-control-allow-credentials" not in response.headers:
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+    if "access-control-allow-methods" not in response.headers:
+        response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+    if "access-control-allow-headers" not in response.headers:
+        response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
 
 # Health endpoint
 
